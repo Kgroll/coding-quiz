6ba571db8ca2f6aed1a335d88c4831e9db543bb5
Kristen Groller <groller@rogers.com>
2021-01-12 19:21:30 -0500

    trying to get the timer to stop

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+ //WHEN I answer a question
+//THEN I am presented with another question
+//WHEN I answer a question incorrectly
+//THEN time is subtracted from the clock
+//WHEN all questions are answered or the timer reaches 0
+//THEN the game is over
+//WHEN the game is over
+//THEN I can save my initials and score
+// creating an array and passing the number, questions, options, and answers
+
 
 $(document).ready(function() {  
     var welcomeBox = $("#welcome");
@@ -5,8 +15,9 @@ $(document).ready(function() {
     var questionContainer = $("#question");
     var questionContainerBtn = $("#btn0, #btn1, #btn2, #btn3");
     var addProgress = $("#progress");
-    var startTimer = $("#countdown");
-    var endingScoreBox = $("#endingScoreBox");
+    var questionIndex = 0;
+    var timerInterval = ">= 0";
+    
     var highScoresBox = $("#highScores");
     var scores = JSON.parse(localStorage.getItem("scores") || "[]");
     var highScoresBtn = $("#highScoreBtn");
@@ -16,8 +27,9 @@ welcomeBox.show();
 questionContainer.hide();
 questionContainerBtn.hide();
 addProgress.hide();
-//start quiz
-//startQuiz.onclick = startTimer;
+
+//start quiz and timer
+
 startQuizBtn.click(function() {
     welcomeBox.hide();    
    questionContainer.show();
@@ -29,7 +41,7 @@ startQuizBtn.click(function() {
 //THEN a timer starts and I am presented with a question
 
 //timer
-var timeLeft = 60
+var timeLeft = 5
 var timer = document.getElementById("timer")
 timer.innerHTML = timeLeft;
 //button on click
@@ -37,52 +49,27 @@ var button = document.getElementById("startQuiz")
 button.addEventListener("click", function() {
 
     var clock = setInterval(function() {
-
+    var captureUserScore = timeLeft
         timeLeft--;
-        timer.innerHTML = timeLeft;
-        if (timeLeft === 0) {
-            clearInterval(clock);
+        timer.textContent = "";
+        timer.textContent = "Time: " + timeLeft;
+        if (timeLeft === 0 || questionIndex.length) {
+            clearInterval()
+            if (timeLeft >= 0)
+            console.log(showScores);
         }
         }, 1000);
     })
 });
 
- 
- //WHEN I answer a question
-//THEN I am presented with another question
-//WHEN I answer a question incorrectly
-//THEN time is subtracted from the clock
-//WHEN all questions are answered or the timer reaches 0
-//THEN the game is over
-//WHEN the game is over
-//THEN I can save my initials and score
-// creating an array and passing the number, questions, options, and answers
 
-//quiz questions start
+//quiz questions start this must stay
 
 function Quiz(questions) {
     this.score = 0;
     this.questions = questions;
     this.questionIndex = 0;
-}
- 
-Quiz.prototype.getQuestionIndex = function() {
-    return this.questions[this.questionIndex];
-}
- 
-Quiz.prototype.guess = function(answer) {
-    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
-        this.score++;
-    }
- 
-    this.questionIndex++;
-}
- 
-Quiz.prototype.isEnded = function() {
-    return this.questionIndex === this.questions.length;
-}
- 
- 
+} 
 function Question(text, choices, answer) {
     this.text = text;
     this.choices = choices;
@@ -92,8 +79,7 @@ function Question(text, choices, answer) {
 Question.prototype.isCorrectAnswer = function(choice) {
     return this.answer === choice;
 }
- 
- 
+  
 function populate() {
     if(quiz.isEnded()) {
         showScores();
@@ -135,23 +121,11 @@ function showScores() {
     gameOverHTML += "<h2 id='score'> Your score is: " + quiz.score + "</h2>";
     var element = document.getElementById("quiz");
     element.innerHTML = gameOverHTML;
-};
+
  
-// create questions here
-var questions = [
-    new Question("Hyper Text Markup Language Stand For?", ["JavaScript", "XHTML","CSS", "HTML"], "HTML"),
-    new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
-    new Question("Which is not a JavaScript Framework?", ["Python Script", "JQuery","Django", "NodeJS"], "Django"),
-    new Question("Which is used for Connect To Database?", ["PHP", "HTML", "JS", "All"], "PHP"),
-    new Question("Webdevtrick.com is about..", ["Web Design", "Graphic Design", "SEO & Development", "All"], "All")
-    
-];
+
  console.log(showScores)
-// create quiz
-var quiz = new Quiz(questions);
- 
-// display quiz
-populate();
+
 //start.addEventListener("click", startQuiz);
 
 function isCorrectAnswer() {
@@ -172,16 +146,16 @@ function isCorrectAnswer() {
         displayQuestion();
         console.log("finish isCorrectAnswer");
       }      
-      var secondsRemaining = questions.length * 5   
+     // var secondsRemaining = questionIndex  
       function removeTime() {
         secondsRemaining -= 15;
       }
-     
+     clearTimeout();
       function getScore() {
         return secondsRemaining;
       }
-      localStorage.setItem("Score", secondsRemaining);
-      
+     // localStorage.setItem("Score", endingScore);
+    }
       function getUserInfo() {}
       
       function saveScore() {}
@@ -206,8 +180,8 @@ function isCorrectAnswer() {
  //getUserName();
 
  //function displayHighscores() {
-    // document.getElementById("Initials").innerHTML = localStorage.getItem("Initials");
-    // document.getElementById("scores").innerHTML = localStorage.getItem("Scores");
+//document.getElementById("Initials").innerHTML = localStorage.getItem("Initials");
+     //document.getElementById("scores").innerHTML = localStorage.getItem("Scores");
      
 
 //displayHighscores();
---
+++ script2.js
@@ -0,0 +1,41 @@
+
+
+
+
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
+}
+//answer buttons
+Quiz.prototype.guess = function(answer) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++;
+    } 
+    this.questionIndex++; //this allows you to guess
+}
+ 
+Quiz.prototype.isEnded = function() {
+    return this.questionIndex === this.questions.length;
+}
+
+
+
+
+
+
+
+
+
+// create questions here
+var questions = [
+    new Question("Hyper Text Markup Language Stand For?", ["JavaScript", "XHTML","CSS", "HTML"], "HTML"),
+    new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
+    new Question("Which is not a JavaScript Framework?", ["Python Script", "JQuery","Django", "NodeJS"], "Django"),
+    new Question("Which is used for Connect To Database?", ["PHP", "HTML", "JS", "All"], "PHP"),
+    new Question("Webdevtrick.com is about..", ["Web Design", "Graphic Design", "SEO & Development", "All"], "All")
+    
+];
+// create quiz
+var quiz = new Quiz(questions);
+ 
+// display quiz
+populate();
